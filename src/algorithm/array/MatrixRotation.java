4299ea45791c3488e130d4f40e7089521b1d500c
jeevkulk <jeevanakulkarni@gmail.com>
2017-07-13 09:15:59 +0530

    corrected matrix rotation problem

--- src/algorithm/array/MatrixRotation.java
+++ src/algorithm/array/MatrixRotation.java
@@ -5,7 +5,7 @@ public class MatrixRotation {
 
     public static void main(String[] args) {
         MatrixRotation matrixRotation = new MatrixRotation();
-        int[][] matrix = {{2, 3, 4}, {5, 6, 7}, {8, 9, 1}};
+        int[][] matrix = {{2, 3, 4, 5}, {6, 7, 8, 9}, {1, 2, 3, 4}, {5, 6, 7, 8}};
         System.out.println("Input matrix");
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[i].length; j++) {
@@ -37,19 +37,23 @@ public class MatrixRotation {
      * Space complexity: O(1)
      */
     private int[][] matrixRotationUsingSwap(int[][] matrix) {
-        int temp, k, l, m, n;
-        m = (matrix.length % 2 == 0) ? (matrix.length / 2) : (matrix.length / 2) + 1;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j <= i; j++) {
-                System.out.println("i="+i+" j="+j);
-                k = matrix.length - i - 1;
-                l = matrix.length - j - 1;
-                System.out.println("k="+k+" l="+l);
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[k][j];
-                matrix[k][j] = matrix[k][l];
-                matrix[k][l] = matrix[i][l];
-                matrix[i][l] = temp;
+        int temp, limit, lefti, leftj, righti, rightj, topi, topj, bottomi, bottomj;
+        limit = (matrix.length / 2) + 1;
+        for(int i = 0; i < limit; i++) {
+            for(int j = 0; j <= i && j < (limit - i); j++) {
+                lefti = i;
+                leftj = j;
+                bottomi = matrix.length - 1 - j;
+                bottomj = i;
+                righti = matrix.length - 1 - i;
+                rightj = matrix.length - 1 - j;
+                topi = j;
+                topj = matrix.length - 1 - i;
+                temp = matrix[lefti][leftj];
+                matrix[lefti][leftj] = matrix[bottomi][bottomj];
+                matrix[bottomi][bottomj] = matrix[righti][rightj];
+                matrix[righti][rightj] = matrix[topi][topj];
+                matrix[topi][topj] = temp;
             }
         }
         return matrix;
