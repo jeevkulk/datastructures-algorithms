bbfb29de2f6414ebee9ea8a5c14b0b125595a528
jeevkulk <jeevanakulkarni@gmail.com>
2017-07-22 14:31:58 +0530

    added test case for MatrixRotation

--- src/main/java/algorithm/array/MatrixRotation.java
+++ src/main/java/algorithm/array/MatrixRotation.java
@@ -3,27 +3,7 @@ package algorithm.array;
 
 public class MatrixRotation {
 
-    public static void main(String[] args) {
-        MatrixRotation matrixRotation = new MatrixRotation();
-        int[][] matrix = {{2, 3, 4, 5}, {6, 7, 8, 9}, {1, 2, 3, 4}, {5, 6, 7, 8}};
-        System.out.println("Input matrix");
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                System.out.print("    "+matrix[i][j]);
-            }
-            System.out.println("");
-        }
-        matrix = matrixRotation.doMatrixRotation(matrix, MatrixRotationAlgorithms.SWAP);
-        System.out.println("Rotated matrix");
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                System.out.print("    "+matrix[i][j]);
-            }
-            System.out.println("");
-        }
-    }
-
-    private int[][] doMatrixRotation(int[][] matrix, MatrixRotationAlgorithms matrixRotationAlgorithms) {
+    public int[][] doMatrixRotation(int[][] matrix, MatrixRotationAlgorithms matrixRotationAlgorithms) {
         switch (matrixRotationAlgorithms) {
             case SWAP:
                 return matrixRotationUsingSwap(matrix);
