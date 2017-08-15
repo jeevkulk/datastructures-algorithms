ed7c3683a8dad927762e804271985f45f35e32ed
jeevkulk <jeevanakulkarni@gmail.com>
2017-08-15 23:45:59 +0530

    Some Hackerrank problems

--- src/main/java/algorithm/array/MaxSumOfSubArray.java
+++ src/main/java/algorithm/array/MaxSumOfSubArray.java
@@ -1,8 +1,48 @@
 package algorithm.array;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class MaxSumOfSubArray {
 
-	public int findMaxSumOfSubArray(int[] inputArr) {
+    Logger logger = LogManager.getLogger(MaxSumOfSubArray.class);
+
+    public int findMaxSumOfSubArray(int[] inputArr, Algorithm algorithm) {
+        int maxSum = Integer.MIN_VALUE;
+        switch (algorithm) {
+            case MY_ALGORITHM:
+                maxSum = findMaxSumOfSubArrayMyAlgo(inputArr);
+                break;
+            case KADANES_ALGORITHM:
+                maxSum = findMaxSumOfSubArrayKadanesAlgo(inputArr);
+                break;
+        }
+        return maxSum;
+
+    }
+
+	public int findMaxSumOfSubArrayKadanesAlgo(int[] inputArr) {
+		int maxSum = Integer.MIN_VALUE, maxSumLowerIndex = 0, maxSumUpperIndex = 0;
+		int currSubArraySum = 0, currSubArrayLowerIndex = 0;
+
+		for (int i = 0; i < inputArr.length; i++) {
+            currSubArraySum = currSubArraySum + inputArr[i];
+            if (maxSum < currSubArraySum) {
+                maxSum = currSubArraySum;
+                maxSumLowerIndex = currSubArrayLowerIndex;
+                maxSumUpperIndex = i;
+            }
+		    if (currSubArraySum < 0) {
+                currSubArraySum = 0;
+                currSubArrayLowerIndex = i+1;
+            }
+		}
+        logger.info("Using Kadane's Algorithm: maxSum = "+maxSum+", maxSumLowerIndex = "+maxSumLowerIndex+", maxSumUpperIndex = "+maxSumUpperIndex);
+		return maxSum;
+	}
+
+	public int findMaxSumOfSubArrayMyAlgo(int[] inputArr) {
 		int maxSum = 0, maxSumLowerIndex = 0, maxSumUpperIndex = 0;
 		int currSubArraySum = 0, currSumLowerIndex = 0, currSumUpperIndex = 0;
 
@@ -33,6 +73,12 @@ public class MaxSumOfSubArray {
                 }
             }
 		}
+        logger.info("Using my algorithm: maxSum = "+maxSum+", maxSumLowerIndex = "+maxSumLowerIndex+", maxSumUpperIndex = "+maxSumUpperIndex);
 		return maxSum;
 	}
+
+	enum Algorithm {
+        MY_ALGORITHM,
+        KADANES_ALGORITHM
+    }
 }
--- src/test/java/algorithm/array/MaxSumOfSubArrayTest.java
+++ src/test/java/algorithm/array/MaxSumOfSubArrayTest.java
@@ -19,15 +19,21 @@ public class MaxSumOfSubArrayTest {
     @Test
     public void testMaxSumOfSubArray() {
         int[] inputArr = new int[]{-1, 2, 3, -4, 9, 2, -1, 10};
-        int sum = maxSumOfSubArray.findMaxSumOfSubArray(inputArr);
+        int sum = maxSumOfSubArray.findMaxSumOfSubArray(inputArr, MaxSumOfSubArray.Algorithm.MY_ALGORITHM);
+        Assert.assertEquals(21, sum);
+        sum = maxSumOfSubArray.findMaxSumOfSubArray(inputArr, MaxSumOfSubArray.Algorithm.KADANES_ALGORITHM);
         Assert.assertEquals(21, sum);
 
         inputArr = new int[]{0, 10, -50, 25, -10, 35, 20, -60};
-        sum = maxSumOfSubArray.findMaxSumOfSubArray(inputArr);
+        sum = maxSumOfSubArray.findMaxSumOfSubArray(inputArr, MaxSumOfSubArray.Algorithm.MY_ALGORITHM);
+        Assert.assertEquals(70, sum);
+        sum = maxSumOfSubArray.findMaxSumOfSubArray(inputArr, MaxSumOfSubArray.Algorithm.KADANES_ALGORITHM);
         Assert.assertEquals(70, sum);
 
         inputArr = new int[]{0, -110, -50, 25, -10, 10, 20, -60};
-        sum = maxSumOfSubArray.findMaxSumOfSubArray(inputArr);
+        sum = maxSumOfSubArray.findMaxSumOfSubArray(inputArr, MaxSumOfSubArray.Algorithm.MY_ALGORITHM);
+        Assert.assertEquals(45, sum);
+        sum = maxSumOfSubArray.findMaxSumOfSubArray(inputArr, MaxSumOfSubArray.Algorithm.KADANES_ALGORITHM);
         Assert.assertEquals(45, sum);
     }
 }
