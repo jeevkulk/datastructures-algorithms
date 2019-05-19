033db92c53014081ec0e1a2783dc68ae51e64ff0
jeevkulk <jeevanakulkarni@gmail.com>
2019-05-19 23:06:49 +0530

    Optimized bubble sort algo

--- src/main/java/algorithm/sort/BubbleSort.java
+++ src/main/java/algorithm/sort/BubbleSort.java
@@ -3,17 +3,19 @@ package algorithm.sort;
 public class BubbleSort<T extends Comparable<T>> {
 
     /**
-     * Bubble sort: Has time complexity O(N^2), space complexity O(1)
+     * Bubble sort:
+     * -> It has time complexity O(N^2), space complexity O(1)
+     * -> Is stable sorting algorithm - which means that equal value elements retain their position w.r.t. each other
      * @param arr
      * @return
      */
     public T[] bubbleSort(T[] arr) {
-        for (int i=0; i<arr.length; i++) {
-            for (int j=i; j<arr.length-1; j++) {
-                if (arr[j].compareTo(arr[j+1]) > 0) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length - i - 1; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
                     T temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
